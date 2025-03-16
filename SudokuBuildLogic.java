c6e570dec927e65796ed7b45dcad1ff6ff4623c7
Krishna Kumar Gupta <148635343+Krishna-KumarGupta@users.noreply.github.com>
2025-03-16 16:59:55 +0530

    Add files via upload

---
+++ SudokuBuildLogic.java
@@ -0,0 +1,35 @@
+package buildlogic;
+
+import computationlogic.GameLogic;
+import persistence.LocalStorageImpl;
+import problemdomain.IStorage;
+import com.wiseassblog.sudokuproblemdomain.SudokuGame;
+import userinterface.IUserInterfaceContract;
+import userinterface.logic.ControlLogic;
+
+import java.io.IOException;
+
+public class SudokuBuildLogic {
+
+    public static void build(IUserInterfaceContract.View userInterface) throws IOException {
+        SudokuGame initialState;
+        IStorage storage = new LocalStorageImpl();
+
+        try {
+            //will throw if no game data is found in local storage
+
+            initialState = storage.getGameData();
+        } catch (IOException e) {
+
+            initialState = GameLogic.getNewGame();
+            //this method below will also throw an IOException
+            //if we cannot update the game data. At this point
+            //the application is considered unrecoverable
+            storage.updateGameData(initialState);
+        }
+
+        IUserInterfaceContract.EventListener uiLogic = new ControlLogic(storage, userInterface);
+        userInterface.setListener(uiLogic);
+        userInterface.updateBoard(initialState);
+    }
+}
